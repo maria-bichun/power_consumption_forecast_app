cffe1046baf6dbdc5abbdff1cae88dbbd2a55755
Maria Bichun <may88@mail.ru>
2023-12-02 00:28:49 +0500

    upgrade to streamlit 1.29

--- utils/df_getter.py
+++ utils/df_getter.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def get_df():
     # Initialize connection.
-    conn = st.experimental_connection("postgresql", type="sql")
+    conn = st.connection("postgresql", type="sql")
 
     # Perform query.
     df = pd.DataFrame(conn.query("SELECT * FROM power_cons ;", ttl="10m"))
